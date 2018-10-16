7977da98244fbf83f14774a6d8b1944405bc0d91
hansen.wang <hansen.wang@vipshop.com>
2018-10-16 16:25:23 +0800

    spring cloud config client

---
+++ config/spring-cloud-config-client/src/main/java/org/spring/cloud/config/client/controller/GetConfigController.java
@@ -0,0 +1,21 @@
+package org.spring.cloud.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GetConfigController {
+    
+    @Value("${whoami}")
+    private String whoami;
+    
+    static {
+        System.err.println("fuck...");
+    }
+    
+    @RequestMapping("/whoami")
+    public String getConfig() {
+        return whoami;
+    }
+}
