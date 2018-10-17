3cffc4c490d8a9def827d85b8439160d74e473b9
hansen.wang <hansen.wang@vipshop.com>
2018-10-17 11:09:51 +0800

    add code

--- config/spring-cloud-config-client/src/main/java/org/spring/cloud/config/client/SpringCloudConfigClient.java
+++ config/spring-cloud-config-client/src/main/java/org/spring/cloud/config/client/SpringCloudConfigClient.java
@@ -2,7 +2,16 @@ package org.spring.cloud.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * EnableDiscoveryClient激活对配置中心的支持
+ * 
+ * <p>Description:</p>
+ * @author hansen.wang
+ * @date 2018年10月17日 上午10:27:21
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudConfigClient {
 
--- config/spring-cloud-config-client/src/main/java/org/spring/cloud/config/client/controller/GetConfigController.java
+++ config/spring-cloud-config-client/src/main/java/org/spring/cloud/config/client/controller/GetConfigController.java
@@ -1,10 +1,28 @@
 package org.spring.cloud.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 使用@RefreshScope注解
+ * 在客户端执行/refresh的时候就会更新此类下面的变量值
+ * 
+ * 手动更新GIT中的配置信息
+ * 
+ * #刷新配置
+ * curl http://localhost:8086/refresh -X POST
+ * 
+ * #获取whoami的值
+ * curl http://localhost:8086/whoami
+ * 
+ * <p>Description:</p>
+ * @author hansen.wang
+ * @date 2018年10月16日 下午5:05:53
+ */
 @RestController
+@RefreshScope
 public class GetConfigController {
     
     @Value("${whoami}")
